4de91870a67f3f79f66dc2552f5ffd4fb383757d
TheRajath <sairajath7@gmail.com>
2023-03-18 22:20:41 +0530

    TV Show Search using Axios

---
+++ tv-show-search-axios/app.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('#searchForm');
+
+form.addEventListener('submit', async function (e) {
+
+    e.preventDefault();
+
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } }
+    const response = await axios.get('https://api.tvmaze.com/search/shows', config);
+
+    makeImages(response.data);
+
+    form.elements.query.value = '';
+
+});
+
+const makeImages = (shows) => {
+
+    for (let result of shows) {
+
+        if (result.show.image) {
+
+            const image = document.createElement('IMG');
+            image.src = result.show.image.medium;
+            document.body.append(image);
+        }
+    }
+}
